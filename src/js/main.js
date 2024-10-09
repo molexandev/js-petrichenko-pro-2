05389b3dedf16a83f6806aa51d4f30189a4cd9eb
molexandev01~ <molexandev@gmail.com>
2024-10-09 23:57:24 +0300

    completion of the project

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,13 @@ import forms from './modules/forms';
 import mask from './modules/mask';
 import checkTextInputs from './modules/checkTextInputs';
 import showMoreStyles from './modules/showMoreStyles';
+import calc from './modules/calc';
+import filter from './modules/filter';
+import pictureSize from './modules/pictureSize';
+import accordion from './modules/accordion';
+import burger from './modules/burger';
+import scrolling from './modules/scrolling';
+import drop from './modules/drop';
 
 window.addEventListener('DOMContentLoaded', () => {
    'use strict';
@@ -21,4 +28,11 @@ window.addEventListener('DOMContentLoaded', () => {
    checkTextInputs('[name="name"]');
    checkTextInputs('[name="message"]');
    showMoreStyles('.button-styles', '#styles .row');
+   calc('#size', '#material', '#options', '.promocode', '.calc-price');
+   filter();
+   pictureSize('.sizes-block');
+   accordion('.accordion-heading');
+   burger('.burger-menu', '.burger');
+   scrolling('.pageup');
+   drop();
 });
